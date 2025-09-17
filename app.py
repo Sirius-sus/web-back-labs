dadb4cbaf46a82fed480705cd847034f206d2b37
Иван Стародубцев <starivan2005@gmail.com>
2025-09-17 16:14:09 +0700

    Вывод цветка

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request, redirect
+from flask import Flask, url_for, request, redirect, abort
 import datetime
 app = Flask(__name__)
 
@@ -226,10 +226,19 @@ def error500():
 def internal_error(err):
     return "Внутренняя ошибка сервера", 500
 
-@app.route("/lab2/a")
+@app.route('/lab2/a')
 def a1():
     return 'без слэша'
 
-@app.route("/lab2/a/")
+@app.route('/lab2/a/')
 def a2():
     return 'со слэшем'
+
+flower_list = ('роза', 'тюльпан', 'незабудка', 'ромашка')
+
+@app.route('/lab2/flowers/<int:flower_id>')
+def flowers(flower_id):
+    if flower_id >= len(flower_list):
+        abort(404)
+    else:
+        return 'id=' + flower_list[flower_id]
