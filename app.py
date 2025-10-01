622633f157d10883d39080f862a01010c317b62d
Иван Стародубцев <starivan2005@gmail.com>
2025-10-01 17:13:59 +0700

    Рефакторинг: выделение первой лабораторной

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, url_for, request, redirect, abort, render_template
 import datetime
+from lab1 import lab1
+
 app = Flask(__name__)
+app.register_blueprint(lab1)
 
 @app.route("/")
 @app.route("/index")
@@ -21,139 +24,6 @@ def index():
     </body>
 </html>
 '''
-
-@app.route("/lab1")
-def lab1():
-    return '''
-<!doctype html>
-<html>
-    <head><title>Лабораторная 1</title></head>
-    <body>
-        <p>
-            Flask — фреймворк для создания веб-приложений на языке
-            программирования Python, использующий набор инструментов
-            Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
-            называемых микрофреймворков — минималистичных каркасов
-            веб-приложений, сознательно предоставляющих лишь самые базовые возможности.
-        </p>
-        <a href="/">Вернуться на главную</a>
-        <h2>Список роутов</h2>
-        <ul>
-            <li><a href="/lab1/web">/lab1/web</a></li>
-            <li><a href="/lab1/author">/lab1/author</a></li>
-            <li><a href="/lab1/image">/lab1/image</a></li>
-            <li><a href="/lab1/counter">/lab1/counter</a></li>
-            <li><a href="/lab1/clear_counter">/lab1/clear_counter</a></li>
-            <li><a href="/lab1/info">/lab1/info</a></li>
-            <li><a href="/lab1/created">/lab1/created</a></li>
-            <li><a href="/400">/400</a></li>
-            <li><a href="/401">/401</a></li>
-            <li><a href="/402">/402</a></li>
-            <li><a href="/403">/403</a></li>
-            <li><a href="/405">/405</a></li>
-            <li><a href="/418">/418</a></li>
-            <li><a href="/500">/500</a></li>
-        </ul>
-    </body>
-</html>
-'''
-
-
-@app.route("/lab1/web")
-def web():
-    return """<!doctype html>
-        <html>
-            <body>
-               <h1>web-сервер на flask</h1>
-               <a href="/lab1/author">author</a>
-            </body>
-        </html>""", 200, {
-            'X-server': 'sample',
-            'Content-Type': 'text/plain; charset=utf-8'
-        }
-
-@app.route("/lab1/author")
-def author():
-    name = "Стародубцев Иван Алексеевич"
-    group = "ФБИ-31"
-    faculty = "ФБ"
-
-    return """<!doctype html>
-        <html>
-            <body>
-               <p>Студент: """ + name + """</p>
-               <p>Группа: """ + group + """</p>
-               <p>Факультет: """ + faculty + """</p>
-               <a href="/lab1/web">web</a>
-            </body>
-        </html>"""
-
-@app.route("/lab1/image")
-def image():
-    path = url_for("static", filename="oak.jpg")
-    css_path = url_for("static", filename="lab1.css")
-    return '''
-<!doctype html>
-<html>
-    <head>
-        <link rel="stylesheet" type="text/css" href="''' + css_path + '''">
-    </head>
-    <body>
-        <h1>Дуб</h1>
-        <img src="''' + path + '''">
-    </body>
-</html>
-''', 200, {
-    'Content-Language': 'ru',
-    'X-Server': 'Flask-Sample',
-    'X-Custom-Header': 'Lab1'
-}
-
-count = 0
-
-@app.route("/lab1/counter")
-def counter():
-    global count
-    count += 1
-    time = datetime.datetime.today()
-    url = request.url
-    client_ip = request.remote_addr
-
-    return '''
-<!doctype html>
-<html>
-    <body>
-        Сколько раз вы сюда заходили: ''' + str(count) + '''
-        <hr>
-        Дата и время: ''' + str(time) + '''<br>
-        Запрошенный адрес: ''' + url + '''<br>
-        Ваш IP-адрес: ''' + client_ip + '''<br>
-        <a href="/lab1/clear_counter">Очистить счётчик</a>
-    </body>
-</html>
-'''
-
-@app.route("/lab1/clear_counter")
-def clear_counter():
-    global count
-    count = 0
-    return redirect("/lab1/counter")
-
-@app.route("/lab1/info")
-def info():
-    return redirect("/lab1/author")
-
-@app.route("/lab1/created")
-def created():
-    return '''
-<!doctype html>
-<html>
-    <body>
-        <h1>Создано успешно</h1>
-        <div><i>Что-то создано...</i></div>
-    </body>
-</html>
-''', 201
 
 log_404 = []
 
