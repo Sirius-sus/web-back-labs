5462e2261e1088ac8b98d2f3264f508dec1d1412
Иван Стародубцев <starivan2005@gmail.com>
2025-11-12 16:15:53 +0700

    Заготовка к 6-ой лабораторной

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
+from lab6 import lab6
 
 app = Flask(__name__)
 
@@ -17,6 +18,7 @@ app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
+app.register_blueprint(lab6)
 
 @app.route("/")
 @app.route("/index")
@@ -35,6 +37,7 @@ def index():
             <li><a href="/lab3">Третья лабораторная</a></li>
             <li><a href="/lab4">Четвёртая лабораторная</a></li>
             <li><a href="/lab5">Пятая лабораторная</a></li>
+            <li><a href="/lab6">Шестая лабораторная</a></li>
         </ul>
         <footer>Стародубцев Иван Алексеевич, ФБИ-31, 3 курс, 2025</footer>
     </body>
