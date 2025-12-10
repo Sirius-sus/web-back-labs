8bcb705e935bec5f5ef934f923d00a9cbba1ae77
Иван Стародубцев <starivan2005@gmail.com>
2025-12-10 12:36:41 +0700

    Автоматический логин

--- lab8.py
+++ lab8.py
@@ -39,6 +39,9 @@ def register():
     new_user = users(login=login_form, password=password_hash)
     db.session.add(new_user)
     db.session.commit()
+
+    login_user(new_user, remember=False)
+
     return redirect('/lab8/')
 
 @lab8.route('/lab8/login', methods=['GET', 'POST'])
