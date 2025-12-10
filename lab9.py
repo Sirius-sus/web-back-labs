fcf440565635cbc8cd349108250fd09785887e89
Иван Стародубцев <starivan2005@gmail.com>
2025-12-10 16:21:36 +0700

    Подарки

--- lab9.py
+++ lab9.py
@@ -1,7 +1,53 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify, session, redirect
+from db import db
+from db.models import gift_box
 
 lab9 = Blueprint('lab9', __name__)
 
 @lab9.route('/lab9/')
 def main():
-    return render_template('lab9/index.html')
+    boxes = gift_box.query.all()
+    unopened_count = gift_box.query.filter_by(is_opened=False).count()
+
+    if 'opened_count' not in session:
+        session['opened_count'] = 0
+
+    return render_template('lab9/index.html',
+                           boxes=boxes,
+                           unopened_count=unopened_count)
+
+
+@lab9.route('/lab9/open_box', methods=['POST'])
+def open_box():
+    data = request.get_json()
+    box_id = data.get('box_id')
+
+    box = gift_box.query.get(box_id)
+    if not box:
+        return jsonify({'error': 'not found'}), 404
+
+    if box.is_opened:
+        return jsonify({'already_opened': True})
+
+    if 'opened_count' not in session:
+        session['opened_count'] = 0
+
+    if session['opened_count'] >= 3:
+        return jsonify({'limit_exceeded': True})
+
+    box.is_opened = True
+    db.session.commit()
+
+    session['opened_count'] += 1
+
+    return jsonify({'success': True,
+                    'redirect_url': f'/lab9/congratulation/{box_id}'})
+
+
+@lab9.route('/lab9/congratulation/<int:box_id>')
+def congratulation(box_id):
+    box = gift_box.query.get(box_id)
+    img_path = f'/static/lab9/congratulation_{box_id}.jpg'
+    return render_template('lab9/congratulation.html',
+                           img_path=img_path,
+                           message=box.message)
